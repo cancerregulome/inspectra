ef73fee63031275e654b063de91a5d177f89d4cd
Dick Kreisberg <rkreisberg@systemsbiology.org>
2013-07-31 16:48:04 -0700

    filter the graphs more completely

--- python/cross_graphs.py
+++ python/cross_graphs.py
@@ -17,36 +17,53 @@ def readGraphJson(json_file):
 
 def rectifyGraphs(graph1, graph2):
 
+   nodes_obj = []
+   nodes = []
    extra_ind = len(graph1[2])
    graph2_fiedler = [0] * extra_ind
    label_map = {}
 
-   for index in range(0,len(graph2[2])):
-	val = graph2[2][index]
-	if val in graph1[2]:
-		position = graph1[2].index(val)
-		label_map[index] = position
-		graph2_fiedler[position] = graph2[0][index]
-	else:
-		graph1[2].append(val)
-		label_map[index] = extra_ind
-		graph2_fiedler.append(graph2[0][index])
-		extra_ind = extra_ind + 1
+   for index in range(0,len(graph2[2])):  # for all nodes in second graph
+	val = graph2[2][index]  # label of node in second graph
+	if val in graph1[2]:	# if node is also in first graph
+		index1 = graph1[2].index(val)
+		nodes_obj.append({ 'x' : graph1[0][index1], 'y' :  graph2[0][index], 'id' : val })  #keep the node
+		nodes.append(val)
 
-   adj = graph1[1]
-   for val in graph2[1]:
-	source = int(label_map[val[0]])
-	target = int(label_map[val[1]])
-	value = float(val[2])
-	adj.append([source,target,value])
+   adj = []
+   from sets import Set
+   used_nodes = Set()
+   
+   for index in range(0, len(graph1[1]) ):  # for all edges in first graph
+	val = graph1[1][index]
+	source = val[0]
+	target = val[1]
+	weight = float(val[2])
+	if (graph1[2][source] in nodes and graph1[2][target] in nodes):   # if label of nodes is in the new array
+		new_source = nodes.index(graph1[2][source])
+		new_target = nodes.index(graph1[2][target])
+		adj.append([new_source, new_target, weight, 1]) # keep it
+		used_nodes.add(new_source)
+		used_nodes.add(new_target)
 
-   nodes = []
-   obj = {}
-   for index in range(len(graph1[0])):
-	obj = { 'x': graph1[0][index], 'y': graph2_fiedler[index], 'id': graph1[2][index] }
-	nodes.append(obj)
+   for index in range(0, len(graph2[1]) ):  # for all edges in second graph
+	val = graph2[1][index]
+	source = val[0]
+	target = val[1]
+	weight = float(val[2])
+	if (graph2[2][source] in nodes and graph2[2][target] in nodes):   # if label of nodes is in the new array
+		new_source = nodes.index(graph2[2][source])
+		new_target = nodes.index(graph2[2][target])
+		adj.append([new_source, new_target, weight, 2]) # keep it
+		used_nodes.add(new_source)
+		used_nodes.add(new_target)
 
-   return { "nodes" : nodes, "edges" : adj}
+#   print "Shrinking node array.\n Current size: ", len(nodes_obj)
+#   filtered_nodes = [ nodes_obj[index] for index in range(0, len(nodes_obj) ) if index in used_nodes ]
+#   print "Shrunk to: " ,len(filtered_nodes)
+
+#   return { "nodes" : filtered_nodes, "edges" : adj}
+   return { "nodes" : nodes_obj, "edges" : adj}
 
 
 def parse_parameters():
@@ -68,7 +85,6 @@ def main():
     graph1 = readGraphJson(args.graph1)
     graph2 = readGraphJson(args.graph2)
     fullGraph = rectifyGraphs(graph1, graph2)
-    print fullGraph['edges']
 
     output = open(args.output, "w")
     json.dump(fullGraph, output)
